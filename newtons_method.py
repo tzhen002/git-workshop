5d6a33127540111492a1422bb75a71d412966d5c
tzhen002 <tzhen002@odu.edu>
2021-09-11 21:35:42 -0400

    Add missing pydoc function docstrings

--- newtons_method.py
+++ newtons_method.py
@@ -14,6 +14,19 @@ def newtons_method(f: Callable[[float], float],
                    df: Callable[[float], float],
                    x_n: float,
                    eps: float = EPSILON) -> Tuple[int, float]:
+    """
+    Compute an approximate solution to f(x) = 0 using Newton's Method.
+
+    Args:
+        f: mathematical function
+        df: derivative of f
+        x_n: an intitial guess (i.e., x_0)
+        eps: tolerance within which two guesses will be considered equal.
+
+    Returns:
+        A Tuple with the number of iterations and approximate solution.
+
+    """
 
     n = 0
 
